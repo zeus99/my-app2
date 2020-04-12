fbf705a7d21afa120b8713300126e35011f52e2a
darksven <tranthitai@gmail.com>
2020-04-12 10:47:26 +0700

    12/04 Push my-app c01

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Course from  './components/Course';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+	render() {
+
+		const items = [
+			{
+				name1: 'ReactJs',
+				time1: '30h',
+				free1: false,
+				desc: "ReactJs is very Simple"
+
+			},
+			{
+				name1: "Angular 4x",
+				time1: "55h",
+				free1: true,
+				desc: "Angular is very Simple"
+
+			},
+			{
+				name1: "NodeJS",
+				time1: "35h",
+				free1: true,
+				desc: "NodeJS is very Simple"
+
+			}
+		];
+
+			const elmCourses = items.map((item,index) =>
+
+			 <Course key= {index} name= {item.name1} time= {item.time1} free= {item.free1} outdate={item.name1} > {item.desc} </Course>
 	
-  return  (
-    
-			  	 
-        <div className="row">
-         <Course name="ReactJs" time="30h" free={true}> React very Simple </Course>
-         <Course name="Angular 4x" time="55h" free={false}/>
-         <Course name ="NodeJS" time="35h" free= {true} / >
-     
-         
-   
-      </div>
-         
-  );
+			);
+
+		  return  (
+		    
+		        <div className="row">
+		            {elmCourses}
+		         
+		        </div>
+		         
+		  );
+
+	}
 }
 
 export default App;
--- src/components/Course.js
+++ src/components/Course.js
@@ -3,40 +3,111 @@ import Lesson from  './Lesson';
 
 class Course extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+
+			isShowOutLine: false,
+			totalStudents: 69
+		};
+
+			
+
+		this.handleClick3= this.handleClick3.bind(this);
+		this.RegisterCourse= this.RegisterCourse.bind(this);
+		this.handleToogleOutLine= this.handleToogleOutLine.bind(this);
+	}
+
+	handleClick1() {
+		alert("view 1");
+
+	}
+
+	handleClick2(content) {
+		alert(content);
+
+	}
+
+	handleClick3(content) {
+		alert(this.props.name);
+
+	}
+
+	RegisterCourse() {
+		//alert("RegisterCourse");
+		console.log(this.refs.username.value);
+	}
+
+	handleToogleOutLine() {
+		this.setState ({isShowOutLine:!this.state.isShowOutLine});
+	}
+
 	 ShowButtonFree() {
+
 	  const isFree = this.props.free;
-	  console.log("isFree",isFree);
+	//  console.log("isFree",isFree);
 	  if (isFree===true) {
-	    return <div className="panel-footer">
-				                  		<button type="button" className="btn btn-warning">View</button>
-				                  	</div>;
+	    return  (
+	  			  <div className="btn-group"> 
+
+          				<button onClick ={this.handleClick1} type="button" className="btn btn-warning"> View 1 </button>
+          				<button onClick = {() => this.handleClick2("view 2")} type="button" className="btn btn-danger"> View 2 </button>
+          				<button onClick = { this.handleClick3} type="button" className="btn btn-success"> View 3</button>
+	   			  </div>
+
+	    	)
+
+          	
+	  }
+	  else {
+
+	  		return(
+
+				<div className="input-group">
+					<span className="input-group-btn">
+						<button onClick={this.RegisterCourse} className="btn btn-info" type="button">Register</button>
+					</span>
+					<input type="text" className="form-control" placeholder="username" ref= "username" />
+				</div>
+
+		
+
+	  		)
 	  }
 	  
 	}
 
 	render() {
-	
-
+		console.log("render");
+		let emlOutLine = null;
+		if(this.state.isShowOutLine)  {
+			emlOutLine = <ul className="list-group">
+				                          <Lesson/>
+				                          <Lesson/>
+				                          <Lesson> </Lesson>
+                   		 </ul>
+		}
 		  return (
 
 		  	<div> 
 				     <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 				              <div className="panel panel-info">
 				                  <div className="panel-heading">
-				                      <h3 className="panel-title">{this.props.name} </h3>
+				                      <h3 className="panel-title">{this.props.name}</h3>
+
 				                  </div>
 				                  <div className="panel-body">
 				                  <p> {this.props.time} </p>
 				                  <p> {this.props.children} </p>
-				                 
-
-				                      <ul className="list-group">
-				                          <Lesson/>
-				                           <Lesson/>
-				                            <Lesson> </Lesson>
-				                      </ul>
+				                  <p> <button onClick = {this.handleToogleOutLine} className="btn btn-success" type="button">Toogle OutLine</button> </p>
+				                  		{emlOutLine}
+				                      
 				                  </div>
-				                {this.ShowButtonFree()}
+
+				                 <div className="panel-footer">
+				               		 {this.ShowButtonFree()}
+				                 </div>
+	
 				                  
 
 
